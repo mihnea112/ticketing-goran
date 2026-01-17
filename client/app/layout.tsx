d9f27baa81a6502e16d5b04fd73d431b72c7d0dd
mihnea112 <ayanna.mihnea@yahoo.com>
2026-01-17 11:30:43 +0200

    facebook pixel setup

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
+import Script from "next/script";
 import { cookies } from "next/headers";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
@@ -34,6 +35,29 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang={initialLang} className={`${inter.variable} ${playfair.variable} scroll-smooth`}>
       <head>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '859893973482297');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=859893973482297&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
